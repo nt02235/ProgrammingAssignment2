d007aa0b7de8458c4c9f3be575228c0634b6f799
nt02235 <nathalie.torres@gmail.com>
2015-10-23 21:57:53 -0400

    Adding comments to cacheMatrix.R for homework submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix will cache the value of a matrix and its inverse,
+# ultimately creating a list of functions for cacheSolve to use
+# cacheSolve will call on the functions created by makeCacheMatrix
+# to determine whether an inverse is cached and if not, calculate
+# the inverse and then cache it
 
-## Write a short comment describing this function
-
+# makeCacheMatrix creates a list of functions when passed a matrix
+# The four functions will save (or "set") the matrix, return (or "get")
+# the matrix, cache (or "setInv") the inverse of the matrix, and lastly
+# return the inverse (or "getInv")
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y) {
@@ -17,8 +22,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+# cacheSolve will first read the cached inverse.  If NULL it will "get"
+# the cached matrix and calculate the inverse, ultimately caching the calculation
+# If an inverse is cached, it will return that value and the function ends
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   i <- x$getInv()
